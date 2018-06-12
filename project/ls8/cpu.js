143cd2a2a7545239831507da06bcbd52cdf45e92
sadrak105 <sadrak10@gmail.com>
2018-06-12 16:05:03 -0400

    print 8

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -5,6 +5,11 @@
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
+const LDI = 0b10011001;
+const PRN = 0b01000011;
+const HLT = 0b00000001;
+
+
 class CPU {
 
     /**
@@ -68,21 +73,47 @@ class CPU {
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the instruction that's about to be executed
         // right now.)
-
+        
         // !!! IMPLEMENT ME
+        const IR = this.ram.read(this.PC);
 
         // Debugging output
-        //console.log(`${this.PC}: ${IR.toString(2)}`);
+        console.log(`${this.PC}: ${IR.toString(2)}`);
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
 
         // !!! IMPLEMENT ME
+        const operandA = this.ram.read(this.PC + 1);
+        const operandB = this.ram.read(this.PC + 2);
 
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
         // !!! IMPLEMENT ME
+
+        switch(IR) {
+            case LDI:
+                // Set the value in a register
+                this.reg[operandA] = operandB;
+                this.PC += 3; //Next Instruction
+                break;
+
+            case PRN:
+                console.log(this.reg[operandA])
+                this.PC += 2;
+                break;
+                
+            case HLT:
+                this.stopClock();
+                this.PC += 1;
+                break;
+
+            default:
+                console.log('Unknown instruction: ' + IR.toString(2));
+                this.stopClock();
+                return;    
+        }
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
